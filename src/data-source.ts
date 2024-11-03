6081db45764d7308fefc5d93a7ddae7c2ef1ddb9
RbOssVF <abransupotito27@gmail.com>
2024-11-03 15:30:46 -0500

    nuevo usuario para la bd

--- src/data-source.ts
+++ src/data-source.ts
@@ -4,8 +4,8 @@ export const AppDataSource = new DataSource({
   type: 'mysql',
   host: '192.168.1.50',
   port: 3306,
-  username: 'rbhome',
-  password: '$817W`Vg3{U|',
+  username: 'ublogcole',
+  password: '19fE6£S1Y@F',
   database: 'blogCole',
   entities: [__dirname + '/**/*.entity{.ts,.js}'],
   migrations: ['src/migration/*.ts'], // Ruta de las migraciones
